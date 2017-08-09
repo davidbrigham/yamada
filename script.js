7b80a8854509bef60e3d7b934cf2be644b1c6bf0
David Brigham <d.brigham@yahoo.com>
2017-08-09 16:38:37 -0600

    fix navigation

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 $(document).ready(function(){
-	$('.box').mouseenter(function() {
-       $(this).animate({
-           height: '+=10vh'
-       });
-   });
-   $('.box').mouseleave(function() {
-       $(this).animate({
-           height: '-=10vh'
-       }); 
-   });
    $('.box').click(function() {
        $(this).toggle(1000);
-   }); 
+   });
+   $('header').click(function(){
+      $(this).effect
+   })
 })
