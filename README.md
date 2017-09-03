942831ea6876f030fc749256bafc8940abb81e35
David Brigham <d.brigham@yahoo.com>
2017-09-03 17:21:37 -0600

    blow up old file structure, moving jekyll stuff to top file to rebuild site from scratch

--- README.md
+++
@@ -1,1 +0,0 @@
-#Yamada_project
--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-   $('.box').click(function() {
-       $(this).toggle(1000);
-   });
-   $('header').click(function(){
-      $(this).effect
-   })
-})
